d886dfc25a0aae46c6b6b28b330071dbaa4eae8c
Matt Hanlon <mhanlon@thecodehub.ie>
2022-06-10 19:50:54 +0100

    Fixing the missing comma.

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .package(url: "https://github.com/mhanlon/MyTimer.git", from: "1.0.0"),
         .package(url: "https://github.com/mhanlon/MyColorPicker.git", from: "1.0.0"),
         .package(url: "https://github.com/mhanlon/AudioMonitor.git", from: "1.0.0"),
-    ]
+    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
